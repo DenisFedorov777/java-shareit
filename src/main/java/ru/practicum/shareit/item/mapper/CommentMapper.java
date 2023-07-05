6ef2894281efad7b2207971caf70d1b50ca9c66c
DenisFedorov <prince.fedorov@yandex.ru>
2023-07-06 03:59:39 +0400

    small 2 fix

--- src/main/java/ru/practicum/shareit/item/mapper/CommentMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/CommentMapper.java
@@ -5,8 +5,6 @@ import lombok.NoArgsConstructor;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.model.Comment;
 
-import java.time.LocalDateTime;
-
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CommentMapper {
 
